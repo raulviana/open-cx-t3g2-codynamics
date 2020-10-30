9cce856906fb7d6823134fb504c18c26c52bc093
Allan Sousa <allanbp.br@gmail.com>
2020-10-30 10:41:52 +0000

    SetUpPage done

--- src/speed_meeting/lib/main.dart
+++ src/speed_meeting/lib/main.dart
@@ -1,111 +1,146 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  final String b = "NANI?!";
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'SpeedMeeting'),
+      title: 'Welcome to Flutter',
+      theme: ThemeData(primaryColor: Colors.red),
+      home: SetUpPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class SetUpPage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _SetUpPageState createState() => _SetUpPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _SetUpPageState extends State<SetUpPage> {
+  final _suggestions = <WordPair>[];
+  final _saved = Set<WordPair>();
+  var _counter = 0;
+  final _biggerFont = TextStyle(fontSize: 18.0);
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Organizer',
-            ),
-            Text(
-             'Atender',
-            ),
+        appBar: AppBar(
+          title: Text('                           Lobby'),
+          actions: [
+            IconButton(icon: Icon(Icons.settings), onPressed: _pushSaved),
           ],
         ),
+        body: new Container(
+            padding: new EdgeInsets.all(32.0),
+            child: new Center(
+              child: new Column(
+
+                children:  <Widget>[
+                  Image(image: AssetImage("images/speed_meeting.jpeg"),),
+                  Padding( padding: EdgeInsets.only(top: 50),
+                      child: new ElevatedButton(onPressed: _pushSaved,
+                          child: Text("Create an Event",textAlign: TextAlign.center,),
+                          style: ButtonStyle(backgroundColor: MaterialStateProperty.resolveWith((states) => Colors.red),
+                            foregroundColor:  MaterialStateProperty.resolveWith((states) => Colors.black),))),
+                  new ElevatedButton(onPressed: _pushSaved,
+                    child: Text("Participate in an Event",textAlign: TextAlign.center,textDirection: TextDirection.ltr,),
+                    style: ButtonStyle(backgroundColor: MaterialStateProperty.resolveWith((states) => Colors.red),
+                      foregroundColor:  MaterialStateProperty.resolveWith((states) => Colors.black),),),
+                  Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children : <Widget>[new ElevatedButton(onPressed: _pushSaved,
+                    child: Text("Edit Profile",textAlign: TextAlign.center,),
+                    style: ButtonStyle(backgroundColor: MaterialStateProperty.resolveWith((states) => Colors.red),
+                        foregroundColor:  MaterialStateProperty.resolveWith((states) => Colors.black)),),
+                    new ElevatedButton(onPressed: _pushSaved,
+                      child: Text("Edit Event",textAlign: TextAlign.center,),
+                      style: ButtonStyle(backgroundColor: MaterialStateProperty.resolveWith((states) => Colors.red),
+                          foregroundColor:  MaterialStateProperty.resolveWith((states) => Colors.black)),)])
+                ],
+              ),
+
+            )));
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: EdgeInsets.all(16.0),
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return Divider(); /*2*/
+
+          final index = i ~/ 2; /*3*/
+          if (index >= _suggestions.length) {
+            for(WordPair wp in generateWordPairs().take(10)) {
+              if (!_suggestions.contains(wp))
+                _suggestions.add(wp);
+            }
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final already_saved = _saved.contains(pair);
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: Icon(   // NEW from here...
+        already_saved ? Icons.favorite : Icons.favorite_border,
+        color: already_saved ? Colors.red : null,
+      ),
+      onTap: () {      // NEW lines from here...
+        setState(() {
+          if (already_saved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+
+  void _pushSaved(){
+    _counter++;
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        // NEW lines from here...
+        builder: (BuildContext context) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Redirect'),
+            ),
+            body: _buildSuggestions()
+          );
+        }, // ...to here.
       ),
     );
   }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
