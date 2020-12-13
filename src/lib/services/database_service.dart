bd0868195be955cc81e18fbb3bd5ba000bd04cb0
a3brx <a3brx@sapo.pt>
2020-12-13 22:25:31 +0000

    added providers and database_service for meetings

--- src/lib/services/database_service.dart
+++ src/lib/services/database_service.dart
@@ -1,10 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:speed_meeting/models/meeting.dart';
 import 'package:speed_meeting/models/user.dart';
 
 class DatabaseService {
   // collection reference
   final CollectionReference _usersCollection =
       FirebaseFirestore.instance.collection("users");
+  final CollectionReference _meetingsCollection =
+      FirebaseFirestore.instance.collection("meetings");
 
   Future saveUser(UserData userData) async {
     //String name, String email, String socialNetwork, List<String> interests
@@ -13,6 +16,15 @@ class DatabaseService {
       'email': userData.email,
       'socialNetwork': userData.socialNetwork,
       'interests': userData.interests,
+    });
+  }
+
+  Future saveMeeting(MeetingData meetingData) async {
+    return await _meetingsCollection.doc(meetingData.uid).set({
+      'name': meetingData.name,
+      'duration': meetingData.duration,
+      'start': meetingData.start,
+      'user': meetingData.users,
     });
   }
 }
